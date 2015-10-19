58f6db031e204d86bffc5066cf799a09bb718a9d
alexbbt <alexander@bell-towne.com>
2015-10-19 15:09:35 -0700

    sorted list

--- index.php
+++ index.php
@@ -5,8 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1.0, user-scalable=0">
-    <title>Police Shootings</title>
-    <link rel="shortcut icon" href="img/badge.png">
+    <title>Info 343 Projects</title>
 
     <!-- jQuery, Bootstrap -->
     <script src="https://code.jquery.com/jquery.min.js"></script>
@@ -14,8 +13,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     
     <!-- Custom files -->
-    <script type="text/javascript" src="js/map.js"></script>
-    <link rel="stylesheet" href="css/main.css"/>
+    <link rel="stylesheet" href="assets/css/main.css"/>
   </head>
   <body>
     <div class="container">
@@ -28,13 +26,33 @@
             <div class="col-xs-10 col-xs-offset-1">
                 <div class="list-group">
 <?php
+    error_reporting(E_ERROR | E_PARSE);
     $dirs = array_filter(glob('*'), 'is_dir');
+    usort($dirs, 'sortDirs');
     foreach ($dirs as $dir) {
         if($dir !== 'assets') {
+            $url = './' . $dir . '/index.html';
+            $doc = new DOMDocument();
+            $doc->strictErrorChecking = FALSE;
+            $doc->loadHTML(file_get_contents($url));
+            $xml = simplexml_import_dom($doc);
+            $arr = $xml->xpath('//link[@rel="shortcut icon"]');
 ?>
-                    <a href="./<?=$dir?>" class="list-group-item"><?=$dir?></a>
+                    <a href="./<?=$dir?>" class="list-group-item">
+<?php
+            if ($arr[0]['href'] != ''){
+?>
+                        <img class="icon" src="<?=$dir . '/' . $arr[0]['href']?>">
+<?
+            }
+            echo $dir;
+?>
+                    </a>
 <?php       
         }
+    }
+    function sortDirs($a, $b) {
+        return (filemtime($a) > filemtime($b));
     }
 ?>                    
                 </div>
